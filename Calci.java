bf20d19c8baf75860354dc4b42cd9979a90a6d74
Devasoth Baloji Naik <137431018+baloji13@users.noreply.github.com>
2023-09-12 15:11:30 +0530

    CalciDev Class is Created

--- Calci.java
+++ Calci.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 interface Calci{
 int add(int a,int b);
 }
@@ -7,5 +8,17 @@ class  CalciImp implements Calci
   int add(int x,int y )
 {
 return x+y;
+}
+}
+public class CalciDev
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter any Two Numbers");
+int n1=s.nextInt();
+int n2=s.nextInt();
+CalciImp c=new CalciImp();
+System.out.println("The Result is"+c.add(n1,n2));
 }
 }
